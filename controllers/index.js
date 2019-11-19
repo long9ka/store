4f9b43d3fd478a9fc0bf3cbb8fa9fcc9f8f4fc4b
long9ka <long9ka@gmail.com>
2019-11-19 22:36:30 +0700

    optimize

--- controllers/index.js
+++ controllers/index.js
@@ -2,20 +2,30 @@ const passport = require('passport');
 
 module.exports = {
     renderPage: (req, res) => {
-        res.send('hello world');
+        res.render('page', { user: req.user.username });
     },
     renderLogin: (req, res) => {
-        res.render('login');
+        res.render('login', { title: 'Login' });
     },
     renderRegister: (req, res) => {
-        res.render('register');
+        res.render('register', { title: 'Register' });
     },
     pageNotFound: (req, res) => {
         res.status(404).render('error', {
+            title: 'Error',
             statusCode: 404,
             message: `This page isn't available`
         });
     },
+    handleLogout: (req, res) => {
+        req.logout();
+        res.redirect('/login');
+    },
+    handleLogin: passport.authenticate('login', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    }),
     handleRegister: passport.authenticate('register', {
         successRedirect: '/',
         failureRedirect: '/register',
