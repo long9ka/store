0090e96fccc4dd69e9206c38672bbb6efa4a506a
long9ka <long9ka@gmail.com>
2019-11-19 18:00:54 +0700

    add modules

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const config = require('./config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
 const app = express();
 
 app.set(express.static('public'));
@@ -15,7 +18,11 @@ app.set('view engine', 'ejs');
 // connect database
 connectDB = async () => {
     try {
-        await mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(config.DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
         console.log('Connected database');
     } catch (error) {
         console.error(error.message);
@@ -24,9 +31,23 @@ connectDB = async () => {
 
 connectDB();
 
+// express body parser
+app.use(flash());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('express-ejs-layouts'));
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+app.use(session({
+    secret: config.SECRET,
+    resave: true,
+    saveUninitialized: true
+}))
+
+// use passport
+require('./middleware/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/terms', require('./routes/terms'));
+app.use('/', require('./routes/index'));
 
 app.listen(config.PORT, console.log(`Server started on port ${config.PORT}`));
